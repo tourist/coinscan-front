5497e82776570b0cea3f66803ea8d8aa5f8a7f79
Michał Oleniec <moleniec@gmail.com>
2022-09-01 01:55:32 +0200

    Refactor chart utils away from hodlers directory.

--- components/Charts/BasicTooltip.test.tsx
+++ components/Charts/BasicTooltip.test.tsx
@@ -1,16 +1,59 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-import { HODLERS_CHART_TOOLTIP_LABEL_FORMATTERS } from '../../components/Holders/HoldersChartTooltip';
-import { HodlersChartGroupings } from '../../components/Holders/consts';
+import {
+  CHART_TIME_TOOLTIP_LABEL_FORMATTERS,
+  ChartTimeGroupings,
+} from '../../utils/charts';
 import BasicTooltip from './BasicTooltip';
 
-test('BalanceChartTooltip renders correctly for daily grouping', () => {
+test('HoldersTooltip renders correctly for daily grouping', () => {
+  const payload = [
+    {
+      payload: { count: 6293, name: '1638144000' },
+    },
+  ];
+
+  const { container } = render(
+    <BasicTooltip
+      active
+      label={1654473600}
+      payload={payload}
+      labelFormatter={
+        CHART_TIME_TOOLTIP_LABEL_FORMATTERS[ChartTimeGroupings.BY_DAY]
+      }
+    />
+  );
+
+  expect(container).toMatchSnapshot('rendered');
+});
+
+test('HoldersTooltip renders correctly for weekly grouping', () => {
+  const payload = [
+    {
+      payload: { count: 5393, name: '1638144000' },
+    },
+  ];
+
+  const { container } = render(
+    <BasicTooltip
+      active
+      label={payload[0].payload.name}
+      payload={payload}
+      labelFormatter={
+        CHART_TIME_TOOLTIP_LABEL_FORMATTERS[ChartTimeGroupings.BY_WEEK]
+      }
+    />
+  );
+
+  expect(container).toMatchSnapshot('rendered');
+});
+
+test('HoldersTooltip renders correctly for monthly grouping', () => {
   const payload = [
     {
       payload: {
-        display: BigInt(123456789112345678),
-        count: 123456789112345678,
-        id: '1635724800',
+        count: 5273,
+        name: '1635724800',
       },
     },
   ];
@@ -18,10 +61,10 @@ test('BalanceChartTooltip renders correctly for daily grouping', () => {
   const { container } = render(
     <BasicTooltip
       active
-      label={payload[0].payload.id}
+      label={payload[0].payload.name}
       payload={payload}
       labelFormatter={
-        HODLERS_CHART_TOOLTIP_LABEL_FORMATTERS[HodlersChartGroupings.BY_DAY]
+        CHART_TIME_TOOLTIP_LABEL_FORMATTERS[ChartTimeGroupings.BY_MONTH]
       }
     />
   );
--- utils/charts.tsx
+++ utils/charts.tsx
@@ -1,9 +1,11 @@
 import dayjs from 'dayjs';
 import isoWeek from 'dayjs/plugin/isoWeek';
 import utc from 'dayjs/plugin/utc';
+import { XAxisProps } from 'recharts';
 
 import { GetTransactionsPaginatedQuery } from '../generated/graphql';
 import settings from '../settings.json';
+import { Props as DefaultTooltipContentProps } from 'recharts/types/component/DefaultTooltipContent';
 
 dayjs.extend(isoWeek);
 dayjs.extend(utc);
@@ -263,4 +265,55 @@ export const formatMax = (
     dataMax = convertBigIntToNumberWithoutDecimalPlacesPrecision(dataMax);
   }
   return dataMax + (roundingBase - (dataMax % roundingBase));
+};
+
+export enum ChartTimeGroupings {
+  BY_DAY,
+  BY_WEEK,
+  BY_MONTH,
+}
+
+export const LINE_CHART_GROUPS = [ChartTimeGroupings.BY_DAY];
+export const BAR_CHART_GROUPS = [
+  ChartTimeGroupings.BY_WEEK,
+  ChartTimeGroupings.BY_MONTH,
+];
+
+type XAxisTickFormatterFn = XAxisProps['tickFormatter'];
+
+type XAxisTickFormatters = {
+  [key in ChartTimeGroupings]: XAxisTickFormatterFn;
+};
+
+export const CHART_TIME_XAXIS_TICK_FORMATTERS: XAxisTickFormatters = {
+  [ChartTimeGroupings.BY_DAY]: (value) =>
+    toLocaleDateStringUTC(fromUnixTime(parseInt(value, 10))),
+  [ChartTimeGroupings.BY_WEEK]: (value) =>
+    toLocaleDateStringUTC(fromUnixTime(parseInt(value, 10))),
+  [ChartTimeGroupings.BY_MONTH]: (value) =>
+    formatUTC(fromUnixTime(parseInt(value, 10)), 'MM.YYYY'),
+};
+
+type TooltipLabelFormatterFn = DefaultTooltipContentProps<
+  string,
+  number
+>['labelFormatter'];
+
+type TooltipLabelFormatters = {
+  [key in ChartTimeGroupings]: TooltipLabelFormatterFn;
+};
+
+export const CHART_TIME_TOOLTIP_LABEL_FORMATTERS: TooltipLabelFormatters = {
+  [ChartTimeGroupings.BY_DAY]: (label: string) =>
+    toLocaleDateStringUTC(fromUnixTime(parseInt(label, 10))),
+  [ChartTimeGroupings.BY_WEEK]: (_, payload) => {
+    const dataDatetime = fromUnixTime(parseInt(payload[0].payload.name, 10));
+    const start = toLocaleDateStringUTC(dataDatetime);
+    const end = toLocaleDateStringUTC(endOfWeek(dataDatetime));
+    return `${start} - ${end}`;
+  },
+  [ChartTimeGroupings.BY_MONTH]: (_, payload) => {
+    const dataDatetime = fromUnixTime(parseInt(payload[0].payload.name, 10));
+    return `${formatUTC(dataDatetime, 'MM.YYYY')}`;
+  },
 };
